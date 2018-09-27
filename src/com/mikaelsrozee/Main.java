ec4777d4a3e7e510bd1a477e335c2ea569a86a71
Mikael Rozee <mikaelsrozee@gmail.com>
2018-09-27 18:25:52 +0100

    Refactoring, separated Player and CheatAI objects.

--- src/com/mikaelsrozee/Main.java
+++ src/com/mikaelsrozee/Main.java
@@ -1,20 +1,18 @@
 package com.mikaelsrozee;
 
-import com.mikaelsrozee.game.Game;
+import com.mikaelsrozee.game.ai.CheatAI;
 import com.mikaelsrozee.game.games.Cheat;
-import com.mikaelsrozee.game.objects.Card;
-import com.mikaelsrozee.game.objects.Player;
 
 public class Main {
 
   public static void main(String[] args) {
     Cheat game = new Cheat();
-    game.addPlayer(new Player("Adam"));
-    game.addPlayer(new Player("Brenda"));
-    game.addPlayer(new Player("Charlie"));
-    game.addPlayer(new Player("Debbie"));
-    game.addPlayer(new Player("Edward"));
-    game.addPlayer(new Player("Florence"));
+    game.addPlayer(new CheatAI("Adam"));
+    game.addPlayer(new CheatAI("Brenda"));
+    game.addPlayer(new CheatAI("Charlie"));
+    game.addPlayer(new CheatAI("Debbie"));
+    game.addPlayer(new CheatAI("Edward"));
+    game.addPlayer(new CheatAI("Florence"));
     game.setDeck(game.getDeck().shuffle());
 
     System.out.println("[LOG] Starting new game of " + game.getId());
